b8fc05a10668bb80851bea8f1171501758d6befa
Agustin <agustinterlaak@gmail.com>
2017-11-20 17:06:28 -0300

    actulizar

---
+++ general_ifyoucan.cpp
@@ -0,0 +1,14 @@
+/*
+Una empresa de serigrafia desea inventar un metodo para desarrolar logos a
+travez ed las iniciales q conforan la firma
+para ellos se ingresan los 3 nombres comppleotos en vectores y
+determinar la letra comun con doble peso
+para determinar las letros  y su peso, basta con tomar una a una cada letra del primer
+ vector y su posicion y encontrar si se encuentra en los otros vecotres con uss respectivas posiciones
+ Se realiza  la suma de las posiciones para las mismmas letras y se ordena de menor a mayor
+ De este modo las tres primeras constituyen  las letras condidatas para el logosPor ej : q sean
+ los tres socios "pedro Alberto","Juan Pablo" y "Maria Alegandra": los vecgores quedarian:
+
+
+ 0 1 2 3 4 5 6 7 8 9 10 11 12 13
+ 
